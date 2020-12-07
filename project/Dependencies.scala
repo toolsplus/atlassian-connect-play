3f75d3d524713159fec2384766fe460fd4f415f1
Scala Steward <74173353+toolsplus-scala-steward@users.noreply.github.com>
2020-12-07 00:47:31 +0000

    Update scalamock to 5.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Version {
   val scalaTest = "3.1.2"
   val scalaTestPlusPlay = "5.1.0"
   val scalaTestPlusScalaCheck = "3.1.2.0"
-  val scalaMock = "4.4.0"
+  val scalaMock = "5.1.0"
   val scalaCheck = "1.14.3"
   val akkaTestKit = "2.6.5"
 }
